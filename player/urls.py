997fc45f0e20340c47f82f902d2becaf8b6e020b
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2023-10-30 23:46:32 -0500

    First commit

---
+++ player/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+# URL Configuration
+urlpatterns = [
+    path('hello/', views.say_hello),
+    path('index/', views.index)
+]
---
+++ player/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+def say_hello(request):
+    x = calculate()
+    return render(request, 'hello.html', {'name': 'Kris'})
+
+def index(request):
+    return render(request, 'index.html')
