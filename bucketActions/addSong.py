1451aded94685cceee3534803814edab4d55fa2f
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2023-12-04 09:12:10 -0600

    Added addSong.py in bucketActions

---
+++ bucketActions/addSong.py
@@ -0,0 +1,16 @@
+import boto3
+from django.conf import settings
+
+
+aws_s3_region_name = 'us-east-2'
+bucket_name = 'lyria-storage'
+s3 = boto3.client('s3', region_name=aws_s3_region_name)
+
+song_name = input("Enter the name of the song you want to add(separate words with underscores): ")
+audio_file = input("Enter the path to the audio file: ")
+image_file = input("Enter the path to the image file: ")
+
+s3.upload_file(audio_file, bucket_name, f"songs/{song_name}/audio_file.mp3")
+s3.upload_file(image_file, bucket_name, f"songs/{song_name}/image_file.jpg")
+
+print("Song added successfully")
