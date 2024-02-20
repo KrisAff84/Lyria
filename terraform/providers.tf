fc6b641cbd512f101638d0ccfa27c33297d9f973
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2024-02-20 16:14:24 -0600

    Migrated terraform state to S3 backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,16 @@
 terraform {
-  required_version = "~> 1.6.5"
+  required_version = "> 1.6.5"
   required_providers {
     aws = {
       version = "~> 5.28.0"
     }
+  }
+  backend "s3" {
+    bucket         = "lyria-terraform-state"
+    encrypt        = true
+    dynamodb_table = "lyria-state-locks"
+    key            = "green/terraform.tfstate"
+    region         = "us-east-2"
+    profile        = "admin-profile"
   }
 }
