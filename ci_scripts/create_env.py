945e4ea9ebd9498a7dea7bfff0e1ca34669a3cc8
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2024-03-18 09:48:05 -0500

    Reformats CI to use .env file for compose
    There was a problem with the compose file not
    recognizing environment variables when using the
    environment parameter. This change uses a python
    script (create_env.py) to create a .env file which
    is then removed after the compose file is run.

---
+++ ci_scripts/create_env.py
@@ -0,0 +1,17 @@
+""" This script creates a .env file to copy to the staging server in the CI pipeline """
+
+import os
+
+env_file = '../.env'
+envs = {
+    'AWS_STORAGE_BUCKET_NAME': os.environ.get('AWS_STORAGE_BUCKET_NAME'),
+    'AWS_S3_REGION_NAME': os.environ.get('AWS_S3_REGION_NAME'),
+    'CLOUDFRONT_URL': os.environ.get('CLOUDFRONT_URL'),
+    'SECRET_KEY': f"'{os.environ.get('SECRET_KEY')}'",
+    'DEBUG': os.environ.get('DEBUG'),
+    'SONG_ORDER': os.environ.get('SONG_ORDER')
+}
+
+with open(env_file, 'x') as f:
+    for key, value in envs.items():
+        f.write(f'{key}={value}\n')
