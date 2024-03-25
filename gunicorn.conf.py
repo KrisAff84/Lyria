5dae3c89f135fe02ad9e559a1d06bf35fca1cbbd
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2024-03-25 00:40:04 -0500

    Configured containers for IPv6
    Updated gunicorn.conf.py and nginx.conf to listen on IPv6 addresses.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 # Install GUnicorn - pip install gunicorn
 # Run - gunicorn -c gunicorn.conf.py lyria.wsgi:application
 
-bind = "0.0.0.0:80"
+bind = "[::]:80"
 workers = 3
 timeout = 60
 module = "lyria.wsgi:application"
