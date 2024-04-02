ef9087c6eda55bf3fca7559c9266cab72afa519a
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2024-04-02 12:33:15 -0500

    Adds Ansible playbook to deploy and start Lyria
    Adds playbook to deploy necessary files and start Lyria on staging
    server. Includes starting gunicorn and nginx. Site is now accessible
    from port 80. Also updated nginx.conf to use www-data as user. Need to
    also configure SSL certificate through CloudFront distribution.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 # Install GUnicorn - pip install gunicorn
 # Run - gunicorn -c gunicorn.conf.py lyria.wsgi:application
 
-bind = "[::]:81"
+bind = "[::]:8000"
 workers = 3
 timeout = 60
 module = "lyria.wsgi:application"
