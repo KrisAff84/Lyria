bcaf0550381f54a80ff74ff867660d94b60ae886
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2024-04-04 12:22:47 -0500

    Brings DNS records under Terrform management
    This commit brings the CNAME DNS records for the CloudFront S3 origin
    under Terraform management. Also, updates terraform/dns_records to use
    variables instead of hard-coded values.

--- terraform/dns_records/main.tf
+++ terraform/dns_records/main.tf
@@ -1,9 +1,19 @@
+provider "aws" {
+  region  = "us-east-2"
+  profile = "admin-profile"
+}
+
+
+#######################################
+# Records for ELB
+#######################################
+
 data "aws_lb" "current" {
   name = "${var.name_prefix}-elb"
 }
 resource "aws_route53_record" "main" {
-  zone_id = "Z09206903VMHX9SR3PGQF"
-  name    = "meettheafflerbaughs.com"
+  zone_id = var.zone_id_main
+  name    = var.A_record_name
   type    = "A"
 
   alias {
@@ -14,8 +24,8 @@ resource "aws_route53_record" "main" {
 }
 
 resource "aws_route53_record" "misspelled" {
-  zone_id = "Z06032811HZJPQ3EPMZ6P"
-  name    = "meetheafflerbaughs.com"
+  zone_id = var.zone_id_misspelled
+  name    = var.A_record_name_misspelled
   type    = "A"
 
   alias {
@@ -25,10 +35,33 @@ resource "aws_route53_record" "misspelled" {
   }
 }
 
+#######################################
+# Records for CloudFront to S3 origin
+#######################################
+
+resource "aws_route53_record" "cf_s3_origin_main" {
+  zone_id = var.zone_id_main
+  name    = var.cf_s3_origin_main_name
+  type    = "CNAME"
+  records = [var.cf_s3_origin_main_record]
+  ttl     = var.cf_s3_origin_ttl
+}
+
+resource "aws_route53_record" "cf_s3_origin_misspelled" {
+  zone_id = var.zone_id_misspelled
+  name    = var.cf_s3_origin_misspelled_name
+  type    = "CNAME"
+  records = [var.cf_s3_origin_misspelled_record]
+  ttl     = var.cf_s3_origin_ttl
+}
+
+#######################################
+# Outputs
+#######################################
+
 output "elb_dns" {
   value = data.aws_lb.current.dns_name
 }
 output "elb_zone_id" {
   value = data.aws_lb.current.zone_id
 }
-
