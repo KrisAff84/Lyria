932dbe35b3b3516ed41c02169ffa5a3fd06a0c7a
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2024-04-06 22:30:57 -0500

    Sets up new staging server for CI Pipeline
    Additionally, makes some other configuration changes, like chaning
    Gunicorn port to 8000, and adding a cache control header to nginx.conf.
    Still messing with terraform/blue, so these changes are not final. Also
    updated .dockerignore to be more streamlined and removes some of the
    files no longer in existence.

--- lyria/urls.py
+++ lyria/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('dh8372jdc/', admin.site.urls),
     path('', include('player.urls'))
 ]
