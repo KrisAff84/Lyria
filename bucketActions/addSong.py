d53a09d8a40ff3b55b219cb3ce945395110d11e0
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2024-04-11 22:04:25 -0500

    Refactors the addSong.py file with correct paths
    Refactors the bucketActions/addSong.py file to use correct paths for dev
    and prod buckets. Additionally, includes ExtraArgs for necessary
    content types.

--- bucketActions/addSong.py
+++ bucketActions/addSong.py
@@ -2,7 +2,10 @@ import boto3
 
 session = boto3.Session(profile_name='kris84')
 aws_s3_region_name = 'us-east-1'
-bucket_names = ['lyria-storage-2024-dev', 'lyria-storage-2024-prod']
+buckets = {
+    'lyria-storage-2024-dev': 'dev/songs/', 
+    'lyria-storage-2024-prod': 'songs/'
+}
 
 s3 = session.client('s3', region_name=aws_s3_region_name)
 
@@ -10,9 +13,9 @@ song_name = input('Enter the name of the song you want to add (uses underscores 
 audio_file = input('Enter the path to the audio file: ')
 image_file = input('Enter the path to the image file: ')
 
-for bucket_name in bucket_names:
-    s3.put_object(Bucket=bucket_name, Key=f'songs/{song_name}/')
-    s3.upload_file(audio_file, bucket_name, f'songs/{song_name}/audio_file.mp3')
-    s3.upload_file(image_file, bucket_name, f'songs/{song_name}/image_file.jpg')
+for bucket, path in buckets.items():
+    s3.put_object(Bucket=bucket, Key=f'{path}{song_name}/')
+    s3.upload_file(audio_file, bucket, f'{path}{song_name}/audio_file.mp3', ExtraArgs={'ContentType': 'audio/mp3'})
+    s3.upload_file(image_file, bucket, f'{path}{song_name}/image_file.jpg', ExtraArgs={'ContentType': 'image/jpg'})
 
-    print(f'Song added successfully to {bucket_name}')
+    print(f'Song successfully added to {bucket}')
